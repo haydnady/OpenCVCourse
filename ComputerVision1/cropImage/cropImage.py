684aa3e99c488801fe5acd0901453d7dbfd411bb
Haydn <haydnady@gmail.com>
2022-01-15 14:24:07 -0500

    Updated

--- ComputerVision1/cropImage/cropImage.py
+++ ComputerVision1/cropImage/cropImage.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 import cv2
 
+
 source = cv2.imread("sample.jpg", 1)
 
 # Make a dummy image, will be useful to clear the drawing.
@@ -11,32 +12,28 @@ dummy = source.copy()
 
 cv2.namedWindow("Window")
 
-
-
 # Lists to store the points
-center=[]
-circumference=[]
-
+pt1 = []
+pt2 = []
 
 def cropImage(action, x, y, flags, userdata):
-  # Referencing global variables
-  global center, circumference
-  # Action to be taken when left mouse button is pressed
-  if action == cv2.EVENT_LBUTTONDOWN:
-    center = [(x, y)]
-    # Mark the center
-    cv2.circle(source, center[0], 1, (255, 255, 0), 2, cv2.LINE_AA)
+    global pt1, pt2
 
-  # Action to be taken when left mouse button is released
-  elif action == cv2.EVENT_LBUTTONUP:
-    circumference = [(x, y)]
-    # Calculate radius of the circle
-    radius = math.sqrt(math.pow(
-        center[0][0]-circumference[0][0], 2)+math.pow(center[0][1]-circumference[0][1], 2))
-    # Draw the circle
-    cv2.circle(source, center[0], int(radius), (0, 255, 0), 2, cv2.LINE_AA)
-    cv2.imshow("Window", source)
+    # Action to be taken when left mouse button is pressed
+    if action == cv2.EVENT_LBUTTONDOWN:
+        pt1 = [(x, y)]
 
+        # Mark top left corner
+        cv2.rectangle(source, pt1[0], pt1[0], (255, 255, 0), thickness=2, lineType=cv2.LINE_8)
+
+
+    # Action to be taken when left mouse button is released
+    elif action == cv2.EVENT_LBUTTONUP:
+        pt2 = [(x, y)]
+
+        # Bottom right corner
+        cv2.rectangle(source, pt1[0], pt2[0], (255, 255, 0), thickness=2, lineType=cv2.LINE_8)
+        cv2.imshow("Window", source)
 
 
 # highgui function called when mouse events occur
